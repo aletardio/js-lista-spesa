c28eaa3c08785a75d5c75d345913af88f2a4658b
aletardio <ale.tardio99@gmail.com>
2023-11-27 14:56:39 +0100

    Created and added script in html, ex. completed

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// CREAZIONE DI UN ARRAY PER LA LISTA DELLA SPESA
+const list = ['Uova', 'Pecorino', 'Guanciale', 'Pepe', 'Pasta'];
+// STAMPA DEL RISULTATO OTTENUTO IN HTML
+const list_container = document.getElementById('shipping-list');
+// DICHIARAZIONE DELLA VARIABILE i
+let i = 0; 
+// REALIZZAZIONE DEL CICLO WHILE
+while(i < list.length) {
+    const li = document.createElement('li');
+    li.innerText = list[i];
+    list_container.appendChild(li);
+    i++;
+}
